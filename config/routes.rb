65e98323c23d7792637997985fd74e74ce8743ea
Ubuntu <ubuntu@ip-172-31-46-191.ap-northeast-1.compute.internal>
2021-03-30 21:40:14 +0000

    sign_in,sign_up,list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # ここにtopコントローラーのindexアクションのルーティングを追加する
   root 'top#index'
+  
+  resources :list, only: %i(new create edit update destroy)
 end
